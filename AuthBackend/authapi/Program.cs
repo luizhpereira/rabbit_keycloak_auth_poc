e2b70966e770808902de109f90281139ab77b4bd
Luiz Henrique Pereira <48666320+luizhpereira@users.noreply.github.com>
2023-07-18 10:49:03 +0100

    Update Program.cs

--- AuthBackend/authapi/Program.cs
+++ AuthBackend/authapi/Program.cs
@@ -4,29 +4,6 @@ using Microsoft.IdentityModel.Tokens;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
-/* JWT Auth */ 
-//builder.Services.AddAuthentication(options =>
-//{
-//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
-//}).AddJwtBearer(options =>
-//{
-//    options.RequireHttpsMetadata = false;
-//    options.Authority = "http://192.168.2.88:8088/realms/usecase";
-
-//    options.TokenValidationParameters = new TokenValidationParameters()
-//    {
-//        ValidateAudience = false,
-//        ValidateIssuerSigningKey = true,
-//        ValidateIssuer = true,
-//        ValidIssuer = "http://192.168.2.88:8088/realms/usecase",
-//        ValidateLifetime = true
-//    };
-//});
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
